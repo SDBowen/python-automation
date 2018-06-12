63211bda5abe16cee8b8167fa1b2ccf5d1508a30
sdBowen <sdbowen.git@gmail.com>
2018-06-12 16:14:46 -0500

    Correct screen change detection

--- lib/menuSelect.py
+++ lib/menuSelect.py
@@ -10,6 +10,6 @@ def menuSelect(menu):
         pyautogui.press('space')
     pyautogui.press('esc')
     # Check for screen change
-    while pyautogui.pixelMatchesColor(320, 160, (255, 255, 255)) == False:
+    while pyautogui.pixelMatchesColor(1700, 200, (255, 255, 255)) == False:
         print('#Check for menu screen: RGB not matched')
     print('On menu screen...')
