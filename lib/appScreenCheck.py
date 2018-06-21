280419f9de08817e0ce0fcce007c6617e1e2446e
sdbowen <sdbowen.git@gmail.com>
2018-06-21 08:25:43 -0500

    Add supplierAdd  logic

--- lib/appScreenCheck.py
+++ lib/appScreenCheck.py
@@ -1,12 +1,12 @@
 import pyautogui
 
 def appScreenCheck(screenType):
-    type_dict = {'report': 'images/report.png', 'freezeUnfreeze': 'images/freeze.png', 'routing': 'images/routing.png', 'element': 'images/itemElement.png', 'overhead': 'images/itemOverhead.png', 'prodStruct': 'images/prodStruct.png', 'dataMaint': 'images/itemMaint.png', 'supplierBrowse': 'images/supplierBrowse.png', 'supplierModify': 'images/supplierModify.png', 'supplierModify2': 'images/supplierModify2.png', 'itemSiteCostMaint': 'images/itemSiteCostMaint.png'}
+    type_dict = {'report': 'images/report.png', 'freezeUnfreeze': 'images/freeze.png', 'routing': 'images/routing.png', 'element': 'images/itemElement.png', 'overhead': 'images/itemOverhead.png', 'prodStruct': 'images/prodStruct.png', 'dataMaint': 'images/itemMaint.png', 'supplierBrowse': 'images/supplierBrowse.png', 'supplierModify': 'images/supplierModify.png', 'supplierModify2': 'images/supplierModify2.png', 'itemSiteCostMaint': 'images/itemSiteCostMaint.png', 'supplierCreate': 'images/supplierCreate.png', 'businessRelationCreate': 'images/businessRelationCreate.png', 'businessRelationAddress': 'images/businessRelationAddress.png', 'businessRelationTab': 'images/businessRelationTab.png'}
     img = type_dict[screenType]
     # Report completion image
-    runComplete = pyautogui.locateOnScreen(img, region=(0, 0 , 1920, 200), grayscale=True)
+    runComplete = pyautogui.locateOnScreen(img, region=(0, 0 , 1920, 240), grayscale=True)
     # Check for menu run completion
     while runComplete == None:
-        runComplete = pyautogui.locateOnScreen(img, region=(0, 0 , 1920, 200), grayscale=True)
+        runComplete = pyautogui.locateOnScreen(img, region=(0, 0 , 1920, 240), grayscale=True)
         print('Waiting for screen match...')
     print('Screen Check: OK!')
--- supplierAdd.py
+++ supplierAdd.py
@@ -22,25 +22,45 @@ time.sleep(1)
 
 clearAppScreen.clearAppScreen()
 
-def makeSupplierInactive():
-    supplierList = []
+cert = 'A'
+supplierName = 'test'
+supplierSearchName = 'test-370-'
+supplierAddressStreet = 'Test'
+supplierAddressZip = '11111'
+supplierAddressCity = 'Test'
+supplierAddressState = 'TX'
+supplierPhone = '1234567'
+supplierFax = '1-234-567-8910'
+supplierEmail = 'test@test.com'
+supplierTerms = 'N60'
+supplierInvoiceStatus = 'S-NOPO'
+supplierTaxId = '11111-11111'
 
+
+
+def createNewSupplier(cert, supplierName, supplierSearchName, supplierAddressStreet, supplierAddressZip, supplierAddressCity, supplierAddressState, supplierPhone, supplierFax, supplierEmail, supplierTerms, supplierInvoiceStatus, supplierTaxId):
+    
     menuSelect.menuSelect('28.20.1.1')
-    appScreenCheck.appScreenCheck('supplierCreate')  # THIS NEEDS TO BE CREATED
+    appScreenCheck.appScreenCheck('supplierCreate') 
     pyautogui.press('tab')
+    pyautogui.keyDown('shift')
+    pyautogui.press('tab')
+    pyautogui.keyUp('shift')
     pyautogui.hotkey('ctrl', 'c')
     for _ in range(2):
         pyautogui.press('tab')
-    pyautogui.press(certification) # NEED VAR ASSIGNMENT
+    pyautogui.press(cert)
     for _ in range(2):
         pyautogui.press('tab')
-    pyautogui.press('space') # NEED CONFIRM SCREEN AFTER THIS
+    pyautogui.press('space') 
+    appScreenCheck.appScreenCheck('businessRelationCreate')
     # Maximize app
     pyautogui.hotkey('alt', ' ', 'x')
-    time.sleep(1) # Screen check instead?
+    appScreenCheck.appScreenCheck('businessRelationAddress')
     pyautogui.hotkey('ctrl', 'v')
     pyautogui.press('tab')
     pyautogui.typewrite(supplierName)
+    pyautogui.press('tab')
     pyautogui.typewrite(supplierSearchName) #28 char
     pyautogui.click(x=1600, y=650, button='right')
     for _ in range(2):
@@ -67,6 +87,7 @@ def makeSupplierInactive():
     pyautogui.press('space')
     pyautogui.press('tab')
     pyautogui.press('space')
+    # accounting
     pyautogui.press('tab')
     for _ in range(2):
         pyautogui.press('right')
@@ -82,16 +103,21 @@ def makeSupplierInactive():
     pyautogui.typewrite('CRF') # CRF EMP N-IC
 
     # Tab selection function
-    x, y = pyautogui.locateCenterOnScreen('images/businessRelationTab.png', grayscale='true')
-    pyautogui.click(x, y)
-    pyautogui.press('right')
-    # accounting
+    appScreenCheck.appScreenCheck('businessRelationTab')
+    x, y = pyautogui.locateCenterOnScreen('images/businessRelationTab.PNG')
+    pyautogui.click(x, (y + 55))
+    for _ in range(2):
+        pyautogui.press('right')
     # payment
     pyautogui.press('tab')
     pyautogui.typewrite(supplierTerms)
     pyautogui.press('tab')    
     pyautogui.typewrite(supplierInvoiceStatus) #S-NOPO, S-POINV
     # banking
+    x, y = pyautogui.locateCenterOnScreen('images/businessRelationTab.PNG')
+    pyautogui.click(x, (y + 55))
+    for _ in range(3):
+        pyautogui.press('right')
     pyautogui.click(x=1600, y=650, button='right')
     for _ in range(4):
         pyautogui.press('up')
@@ -109,12 +135,16 @@ def makeSupplierInactive():
 
     # default
     # tax info
+    x, y = pyautogui.locateCenterOnScreen('images/businessRelationTab.PNG')
+    pyautogui.click(x, (y + 55))
+    for _ in range(5):
+        pyautogui.press('right')
     for _ in range(3):
         pyautogui.press('tab')
     pyautogui.typewrite(supplierTaxId)
     for _ in range(11):
         pyautogui.press('tab')
-    pyautogui.press('space')
+    #pyautogui.press('space')
     print('Done')
 
-makeSupplierInactive()
+createNewSupplier(cert, supplierName, supplierSearchName, supplierAddressStreet, supplierAddressZip, supplierAddressCity, supplierAddressState, supplierPhone, supplierFax, supplierEmail, supplierTerms, supplierInvoiceStatus, supplierTaxId)
